4fa85252db2dada2f213ae8212a018cdc0bfc1c5
jesse <jesse@163.com>
2022-02-08 18:31:42 +0800

    避免路由错误

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,15 @@ import NavFooter from "./../components/NavFooter"
 
 Vue.use(VueRouter);//安装vue-router插件
 
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location, onResolve, onReject) {
+    if (onResolve || onReject) {
+        return originalPush.call(this, location, onResolve, onReject)
+    }
+    return originalPush.call(this,location).catch(err=>err)
+}
+
+
 //将Vue实例作为模板进行导出
 export default new VueRouter({
     routes: [
